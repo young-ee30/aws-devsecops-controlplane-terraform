1c7076fee2af4ea4a1fd51f08e604ba36ba11a96
level-high <jsooha7@gmail.com>
2026-03-19 12:38:23 +0900

    vpc flow log terraform test

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -18,6 +18,12 @@ variable "private_subnet_cidrs" {
   type = list(string)
 }
 
+variable "flow_log_retention_in_days" {
+  description = "VPC Flow Log CloudWatch 보관 주기 (일). 비용 절감을 위해 짧게 유지"
+  type        = number
+  default     = 14
+}
+
 variable "tags" {
   type    = map(string)
   default = {}
